79c4196c6742b9aecfb7277bc7cfba16f5494dd8
Ryan Chalmers <Jaz6629@gmail.com>
2019-09-06 23:45:48 -0400

    added command for quick video call links

---
+++ commands/videocall.js
@@ -0,0 +1,21 @@
+module.exports.run = (client, message) => {
+    if (message.member) {
+        if (message.member.voiceChannel) {
+            message.channel.send(`https://www.discordapp.com/channels/${message.guild.id}/${message.member.voiceChannelID}`);
+        }
+        else {
+            message.channel.send(`**${message.author.tag}** You're not in a voice channel!`);
+        }
+    } else {
+        message.channel.send('Something went wrong idk');
+    }
+}
+
+module.exports.conf = {
+    enabled: true,
+    aliases: ['link', 'video']
+}
+
+module.exports.help = {
+    name: 'videocall'
+}
